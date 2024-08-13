2d780946785b9c238b23e9bdde06bac7eba46d1b
Dilip Babu <106616107+619DILIP@users.noreply.github.com>
2024-08-13 12:07:37 -0400

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class SeleniumTest {
